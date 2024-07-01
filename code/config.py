5295f9008ee8614e20b542c8192e35a412286dbd
IvanWeissVanDerPol <weissvanderpol.ivan@gmail.com>
2024-07-01 11:45:25 -0400

    Update config.py

--- code/config.py
+++ code/config.py
@@ -1,5 +1,4 @@
 import os 
-# API_KEY = "sk-proj-TXNBTI6h8HveEtIorGyST3BlbkFJyHVyIpmhLLnxaUzX9wlu"
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 API_URL = "https://api.openai.com/v1/chat/completions"
 HEADERS = {
